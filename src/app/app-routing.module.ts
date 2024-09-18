64ec3f1f8406927147cab21e2ff84a82cee27d31
Jean <oka.jeanjaures@gmail.com>
2024-09-18 14:03:41 +0200

    feat : add news features and declare news components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SigninComponent} from "./signin/signin.component";
+import {SignupComponent} from "./signup/signup.component";
+import {PasswordComponent} from "./password/password.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'signin',component:SigninComponent},
+  {path:'signup',component: SignupComponent},
+  {path:'password',component:PasswordComponent},
+  { path: "", component: SigninComponent, pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,BrowserModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,58 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { PasswordComponent } from './password/password.component';
+import {RouterModule, RouterOutlet} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {SigninService} from "./signin/signin.service";
+import {SignupService} from "./signup/signup.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {ErrorHandlerService} from "./services/error-handler.service";
+import {ProvidersModule} from "./providers/providers.module";
+import { HomeComponent } from './travelers/home/home.component';
+import {TravelersModule} from "./travelers/travelers.module";
+import {LessorModule} from "./lessor/lessor.module";
+import {PropertyService} from "./services/property.service";
+import {ImageService} from "./services/image.service";
+import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    PasswordComponent,
+    HomeComponent,
+    AsideComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule,
+    RouterOutlet,
+    CommonModule,
+    ProvidersModule,
+    TravelersModule,
+    LessorModule
   ],
-  providers: [],
+  providers: [
+    SigninService,
+    SignupService,
+    ReactiveFormsModule,
+    ErrorHandlerService,
+    PropertyService,
+    ImageService,
+    //DeliveryReqService
+  ],
+  exports: [
+    AsideComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
